db7463ff7ff3d51860070c82ca0d1b95980354cf
Lida Attenborough <attenboroughl@protonmail.com>
2020-07-20 14:21:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,5 +7,12 @@ def find_even_values(src)
     [99, 50, 3, 4],
     [23, 41]
     ]
-
+row_index = 0
+while row_index < src.count do
+  element_index = 0
+  while element_index < scr[row_index].count do
+    element_index +=1
+  end
+  row_index += 1
+end
 end
